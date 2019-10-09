d4d551d5cddae3db0e398c479d562e43a046cf8c
VasileiosKarapoulios <billkarap123@gmail.com>
2019-10-09 21:05:08 +0200

    knapsack_dynamic

--- Lab6/R/brute_force_knapsack.R
+++ Lab6/R/brute_force_knapsack.R
@@ -1,3 +1,5 @@
+library(tictoc)
+
 set.seed(42)
 n <- 2000
 knapsack_objects <-
@@ -6,7 +8,9 @@ knapsack_objects <-
     v=runif(n = n, 0, 10000)
   )
 
+
 brute_force_knapsack <- function(x, W){
+  tic()
   if(!is.data.frame(x) || !is.numeric(W) || W<=0){
     stop()
   }
@@ -33,5 +37,39 @@ brute_force_knapsack <- function(x, W){
   lst <- list()
   lst$value <- round(maxValue)
   lst$elements <- best_id
+  toc()
+  return(lst)
+}
+
+knapsack_dynamic <- function(x, W){
+  n <- length(x[,1])
+  m<-matrix(0, nrow=n,ncol=W)
+  for(i in 2:n){
+    for(j in 1:W){
+      #print(x[j,1])
+      #print(j)
+      if(x[i,1]>j){
+        m[i,j]<-m[i-1,j]
+      }else{
+        m[i,j]<-max(m[i-1,j], m[i-1,j-x[i,1]] + x[i,2])
+      }
+    }
+  }
+  elements <- c()
+  temp_counter <- c(length(x[,1]) , W )
+  for(i in n:2){
+    if(m[i,temp_counter[2]] == m[i-1,temp_counter[2]]) {
+      temp_counter[1] <- temp_counter[1] - 1
+    }
+    else {
+      temp_counter[2] <- temp_counter[2]- x[i,1]
+      temp_counter[1] <- temp_counter[1] - 1
+      elements <- c(elements,i)
+    }
+  }
+  elements<-sort(elements, decreasing = FALSE)
+  lst <- list()
+  lst$value <- round(m[n,W])
+  lst$elements <- elements
   return(lst)
 }
