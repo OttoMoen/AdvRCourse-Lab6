1ece9c05af986a16e03f4671181e51547695e3bf
VasileiosKarapoulios <billkarap123@gmail.com>
2019-10-09 22:10:51 +0200

    roxygen + travis

--- Lab6/R/knapsack_dynamic.R
+++ Lab6/R/knapsack_dynamic.R
@@ -1,3 +1,17 @@
+#' Implementation of the knapsack_dynamic function. 
+#' @name knapsack_dynamic
+#' @param x A dataframe containing two vectors with the weights (w) and the values (v).
+#' @param W The maximum capacity.
+#' @return A list containing the value and the elements used to fill the knapsack.
+#' @description The following algorithm, finds the best combination of the elements in terms of value-weight.
+#' @references \url{https://en.wikipedia.org/wiki/Knapsack_problem}
+#' @import tictoc
+#' @export knapsack_dynamic
+#' @examples
+#' knapsack_dynamic(x = knapsack_objects[1:8,], W = 3500)
+#' knapsack_dynamic(x = knapsack_objects[1:12,], W = 3500)
+#' knapsack_dynamic(x = knapsack_objects[1:8,], W = 2000)
+
 library(tictoc)
 
 set.seed(42)
@@ -14,8 +28,6 @@ knapsack_dynamic <- function(x, W){
   m<-matrix(0, nrow=n,ncol=W)
   for(i in 2:n){
     for(j in 1:W){
-      #print(x[j,1])
-      #print(j)
       if(x[i,1]>j){
         m[i,j]<-m[i-1,j]
       }else{
